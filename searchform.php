2b7d9b3524648fd00035d274eb2fe0a8ec90e12b
Manender Kumar <manender.kumar@mamsys.com>
2019-03-29 18:12:35 +0530

    search box and page design

---
+++ searchform.php
@@ -0,0 +1,10 @@
+<div id="search">
+    <form id="searchform" method="get" action="<?php echo esc_url( home_url( '/' ) ) ?>">
+    <div class="input-group mb-4">
+        <input class="form-control" placeholder="Search..." type="text"  name="s" id="s" size="30">
+            <div class="input-group-append">
+                <span class="input-group-text p-0"><button type="submit" class="btn btn-primary"><i class="ni ni-zoom-split-in"></i></button></span>
+            </div>
+        </div>
+    </form>
+</div>
