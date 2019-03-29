1875104a4fe22628ceeaf47381590da2abe760c5
Manender Kumar <manender.kumar@mamsys.com>
2019-03-29 16:00:30 +0530

    comment form design

--- comments.php
+++ comments.php
@@ -69,7 +69,32 @@ if ( post_password_required() ) {
 
 	endif; // Check for have_comments().
 
-	comment_form();
+	$args = array(
+		'fields' => apply_filters(
+			'comment_form_default_fields', array(
+				'author' => 	'<div class="form-group"> <input id="author" placeholder="Your Name (Required)" class="form-control" name="author" type="text" value="' . 
+								esc_attr( $commenter['comment_author'] ) . '" size="30" required/>'
+								. '</div>',
+				'email'  => 	'<div class="form-group"> <input id="email" placeholder="Your Email (Required)" class="form-control" name="email" type="email" value="' . 
+								esc_attr( $commenter['comment_author_email'] ) . '" size="50" required />'
+								. '</div>',
+				'url'    =>		'<div class="form-group"> <input id="url" placeholder="Your website address" class="form-control" name="url" type="text" value="' . 
+								esc_attr( $commenter['comment_author_url'] ) . '" size="100" />'
+								. '</div>',
+				'cookies'=> 	'<div class="form-group custom-control custom-checkbox">
+									<input class="custom-control-input" id="wp-comment-cookies-consent" type="checkbox">
+									<label class="custom-control-label" for="wp-comment-cookies-consent">
+									<span>Save my name, email, and website in this browser for the next time I comment</span>
+									</label>
+								</div>'
+			)
+		),
+		'comment_field'	=> '<div class="form-group"> <textarea id="comment" placeholder="Your Comment (Required)" class="form-control" name="comment" rows="5" required></textarea></div>',
+		'title_reply' 	=> '<div class="crunchify-text"> <h5>Please Post Your Comments & Reviews</h5></div>',
+		'class_submit'	=> 'btn btn-1 btn-primary',
+		'comment_notes_before' => 'Your email address will not be published'
+	);
+	comment_form($args, $post->ID);
 	?>
 
 </div><!-- #comments -->
