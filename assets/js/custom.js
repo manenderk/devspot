e01a4cf5a187087a0ce6c841115f015c685e7753
Manender Kumar <manender.kumar@mamsys.com>
2019-05-16 15:28:24 +0530

    shortlink dashboard updates

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -194,7 +194,7 @@ $(function(){
 					var links = response['message'];
 					$.each(links, function(key, link){
 						var displayLink = siteUrl + '/' + link['shortLink'];
-						linkBody += '<p><a href="' + displayLink + '">' + displayLink + '</a> - ' + link['redirectLink'] +'</p>';						
+						linkBody += '<p><a href="' + displayLink + '" target="_blank">' + displayLink + '</a> - ' + link['redirectLink'] +'</p>';						
 					})
 					$('#shortlink-list-view').html(linkBody);
 				}
@@ -226,6 +226,19 @@ $(function(){
 		})	
 	})
 
+	var colors = [
+		'#5e72e4',		
+		'#8965e0',		
+		'#f5365c',		
+		'#ffd600',
+		'#fb6340',
+		'#2dce89',
+		'#f3a4b5',
+		'#11cdef',
+		'#5603ad',
+		'#2bffc6'
+	];
+
 	if($('#shortlink-dashboard').length){
 		$.ajax({
 			url: wpApiSettings.root + "dshortlink/v1/get-stats/?_wpnonce=" +wpApiSettings.nonce,
@@ -234,40 +247,20 @@ $(function(){
 			var message = '';
 			if(response['status'] == 'success'){
 				var totalClicks = response['message']['totalClicks'][0]['clicks'];
+				var todayClicks = response['message']['todayClicks'][0]['clicks'];
 				var byClicks = response['message']['byClicks'];
 				var byReferer = response['message']['byReferer'];
 				var byCountry = response['message']['byCountry'];
 				
-				var cTotalClicks = document.getElementById('shortlink-stats-total-clicks').getContext('2d');
-				var chart1 = new Chart(cTotalClicks, {
-					type: 'pie',
-					data: {
-						datasets : [{
-							data: [
-								totalClicks
-							],
-							backgroundColor: [
-								'blue'
-							],
-							label: 'Total Clicks'
-						}],
-						labels: [
-							'Total Clicks'
-						]
-					},
-					options: {
-						responsive: true
-					}
-				});
+				generateTotalClicksContent(totalClicks);
 
-				/*
-				var linkBody = '';
-				var links = response['message'];
-				$.each(links, function(key, link){
-					var displayLink = siteUrl + '/' + link['shortLink'];
-					linkBody += '<p><a href="' + displayLink + '">' + displayLink + '</a> - ' + link['redirectLink'] +'</p>';						
-				})
-				$('#shortlink-list-view').html(linkBody);*/
+				generateTodayClicksContent(todayClicks);
+
+				generateByClicksContent(byClicks);
+
+				generateByRefererContent(byReferer);
+
+				generateByCountryContent(byCountry);
 			}
 			else{
 				message = '<div class="alert alert-danger alert-dismissible fade show" role="alert"><span class="alert-inner--text"><strong>Error!!</strong> ' + response['message'] + '</span><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button></div>';
@@ -276,4 +269,155 @@ $(function(){
 		})
 	}
 
+	function generateTotalClicksContent(totalClicks){
+		var cTotalClicks = document.getElementById('shortlink-stats-total-clicks-chart').getContext('2d');
+		var chart1 = new Chart(cTotalClicks, {
+			type: 'doughnut',
+			data: {
+				datasets: [{
+					data: [
+						totalClicks
+					],
+					backgroundColor: [
+						'#5e72e4'
+					],
+					label: 'Total Clicks'
+				}],
+				labels: [
+					'Total Clicks'
+				]
+			},
+			options: {
+				responsive: true,
+				elements: {
+					center: {
+						text: totalClicks,
+						color: '#5e72e4', // Default is #000000
+						fontStyle: 'Arial', // Default is Arial
+						sidePadding: 40 // Defualt is 20 (as a percentage)
+					}
+				}
+			}
+		});
+		$('#shortlink-stats-total-clicks-number').text(totalClicks);
+	}
+
+	function generateTodayClicksContent(todayClicks) {
+		var cTodayClicks = document.getElementById('shortlink-stats-today-clicks-chart').getContext('2d');
+		var chart5 = new Chart(cTodayClicks, {
+			type: 'doughnut',
+			data: {
+				datasets: [{
+					data: [
+						todayClicks
+					],
+					backgroundColor: [
+						'#5e72e4'
+					],
+					label: 'Today Clicks'
+				}],
+				labels: [
+					'Today Clicks'
+				]
+			},
+			options: {
+				responsive: true,
+				elements: {
+					center: {
+						text: todayClicks,
+						color: '#5e72e4', // Default is #000000
+						fontStyle: 'Arial', // Default is Arial
+						sidePadding: 40 // Defualt is 20 (as a percentage)
+					}
+				}
+			}
+		});
+		$('#shortlink-stats-today-clicks-number').text(todayClicks);
+	}
+
+	function generateByClicksContent(byClicks){
+		var clicks = [];		
+		var shortlinks = [];
+		var chartColors = [];
+		var tableBody = '';
+		$.each(byClicks, function (key, value) {
+			clicks.push(value['clicks']);
+			shortlinks.push(value['shortLink']);
+			chartColors.push(colors[key%10]);
+			tableBody += '<tr><td>' + (key+1) + '</td><td>' + value['shortLink'] + '</td><td>' + value['clicks'] + '</td></tr>';
+		});
+		var cByClicks = document.getElementById('shortlink-stats-by-clicks-chart').getContext('2d');
+		var chart2 = new Chart(cByClicks, {
+			type: 'doughnut',
+			data: {
+				datasets: [{
+					data: clicks,
+					backgroundColor: chartColors
+				}],
+				labels: shortlinks
+			},
+			options: {
+				responsive: true				
+			}
+		})
+		$('#shortlink-stats-by-clicks-table tbody').html(tableBody);
+		
+
+	}
+
+	function generateByRefererContent(byReferer){
+		var clicks = [];
+		var referers = [];
+		var chartColors = [];
+		var tableBody = '';
+		$.each(byReferer, function (key, value) {
+			clicks.push(value['clicks']);
+			referers.push(value['referer']);
+			chartColors.push(colors[key % 10]);
+			tableBody += '<tr><td>' + (key + 1) + '</td><td>' + value['referer'] + '</td><td>' + value['clicks'] + '</td></tr>';
+		});
+		var cByReferer = document.getElementById('shortlink-stats-by-referer-chart').getContext('2d');
+		var chart3 = new Chart(cByReferer, {
+			type: 'doughnut',
+			data: {
+				datasets: [{
+					data: clicks,
+					backgroundColor: chartColors,
+				}],
+				labels: referers
+			},
+			options: {
+				responsive: true
+			}
+		})
+		$('#shortlink-stats-by-referer-table tbody').html(tableBody);
+	}
+
+	function generateByCountryContent(byCountry){
+		var clicks = [];
+		var country = [];
+		var chartColors = [];
+		var tableBody = '';
+		$.each(byCountry, function (key, value) {
+			clicks.push(value['clicks']);
+			country.push(value['country']);
+			chartColors.push(colors[key % 10]);
+			tableBody += '<tr><td>' + (key + 1) + '</td><td>' + value['country'] + '</td><td>' + value['clicks'] + '</td></tr>';
+		});
+		var cByCountry = document.getElementById('shortlink-stats-by-country-chart').getContext('2d');
+		var chart4 = new Chart(cByCountry, {
+			type: 'doughnut',
+			data: {
+				datasets: [{
+					data: clicks,
+					backgroundColor: chartColors,
+				}],
+				labels: country
+			},
+			options: {
+				responsive: true
+			}
+		})
+		$('#shortlink-stats-by-country-table tbody').html(tableBody);
+	}
 })
--- inc/page-partials/shortlink-dashboard.php
+++ inc/page-partials/shortlink-dashboard.php
@@ -1,116 +1,137 @@
 <div class="col-sm-12">
-    <div class="row">
-        <div class="col-lg-3 col-md-4">
-            <div class="list-group" id="list-tab" role="tablist">
-                <a class="list-group-item list-group-item-action active" id="shortlink-dashboard" data-toggle="list" href="#shortlink-dashboard-container" role="tab" aria-controls="overview">Overview</a>
-                <a class="list-group-item list-group-item-action" id="shortlink-list" data-toggle="list" href="#shortlink-list-container" role="tab" aria-controls="your shortlink">Your Shortlinks</a>
-                <a class="list-group-item list-group-item-action" id="shortlink-new" data-toggle="list" href="#shortlink-new-container" role="tab" aria-controls="new shortlink">New Shortlink</a>                                
-            </div>       
-            <br><br>     
-        </div>
-        <div class="col-lg-9 col-md-col-8">
-            <div id="message-container"></div>
-            <div class="tab-content" id="nav-tabContent">
-                <div class="tab-pane fade show active" id="shortlink-dashboard-container" role="tabpanel" aria-labelledby="shortlink-dashboard">
-                    <h5>Overview</h5>
-                    <br>
-                    <div id="shortlink-dashboard-carousel" class="carousel slide" data-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <h6>Total Clicks</h6>
-                                <div class="row">
-                                    <div class="col-md-6" style="height: 100%">
-                                        <canvas id="shortlink-stats-total-clicks" class="shortlink-stats-chart"></canvas>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <table class="table table-hover">
-                                            <thead>
-                                                <tr>
-                                                <th scope="col">#</th>
-                                                <th scope="col">First</th>
-                                                <th scope="col">Last</th>
-                                                <th scope="col">Handle</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                <th scope="row">1</th>
-                                                <td>Mark</td>
-                                                <td>Otto</td>
-                                                <td>@mdo</td>
-                                                </tr>
-                                                <tr>
-                                                <th scope="row">2</th>
-                                                <td>Jacob</td>
-                                                <td>Thornton</td>
-                                                <td>@fat</td>
-                                                </tr>
-                                                <tr>
-                                                <th scope="row">3</th>
-                                                <td colspan="2">Larry the Bird</td>
-                                                <td>@twitter</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <h6>By Clicks</h6>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <canvas id="shortlink-stats-by-clicks" class="shortlink-stats-chart"></canvas>
-                                    </div>
-                                    <div class="col-md-6">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <h6>By Referrer</h6>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <canvas id="shortlink-stats-by-referer" class="shortlink-stats-chart"></canvas>
-                                    </div>
-                                    <div class="col-md-6">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <h6>By Country</h6>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <canvas id="shortlink-stats-by-country" class="shortlink-stats-chart"></canvas>
-                                    </div>
-                                    <div class="col-md-6">
-                                    </div>
-                                </div>
-                            </div>
+    <ul class="nav nav-tabs" id="list-tab" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active" id="shortlink-dashboard" data-toggle="tab" href="#shortlink-dashboard-container" role="tab" aria-controls="overview" aria-selected="true">Overview</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="shortlink-list" data-toggle="tab" href="#shortlink-list-container" role="tab" aria-controls="your shortlink" aria-selected="false">Your Shortlinks</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="shortlink-new" data-toggle="tab" href="#shortlink-new-container" role="tab" aria-controls="new shortlink" aria-selected="false">New Shortlink</a>
+        </li>
+    </ul>
+    <div id="message-container"></div>
+    <div class="tab-content mt-sm" id="list-tab-content">
+        <div class="tab-pane fade show active" id="shortlink-dashboard-container" role="tabpanel" aria-labelledby="shortlink-dashboard">
+            <h5>Overview</h5>                    
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h3>Total Clicks - <span id="shortlink-stats-total-clicks-number" class="font-weight-bold"></span></h3>
+                            <canvas id="shortlink-stats-total-clicks-chart"></canvas>
                         </div>
-                        <a class="carousel-control-prev text-primary" href="#shortlink-dashboard-carousel" role="button" data-slide="prev">
-                            <i class="fa fa-chevron-left fa-3x"></i>
-                        </a>
-                        <a class="carousel-control-next text-primary" href="#shortlink-dashboard-carousel" role="button" data-slide="next">
-                            <i class="fa fa-chevron-right fa-3x"></i>
-                        </a>
+                        <div class="col-md-6">
+                            <h3>Today Clicks - <span id="shortlink-stats-today-clicks-number" class="font-weight-bold"></span></h3>
+                            <canvas id="shortlink-stats-today-clicks-chart"></canvas>
+                        </div>
                     </div>
+                </div>                
+                <div class="col-md-12 mt-sm">
+                    <h3>Distribution by Clicks</h3>
                 </div>
-                <div class="tab-pane fade" id="shortlink-list-container" role="tabpanel" aria-labelledby="shortlink-list">
-                    <h5>Your Shortlinks</h5>
-                    <div id="shortlink-list-view">
-                    </div>
+                <div class="col-md-6">
+                    <canvas id="shortlink-stats-by-clicks-chart"></canvas>
                 </div>
-                <div class="tab-pane fade" id="shortlink-new-container" role="tabpanel" aria-labelledby="shortlink-new">
-                    <h5>New Shortlink</h5>
-                    <form id="add-shortlink-form">
-                        <div class="form-group">
-                            <input type="url" placeholder="Paste your link here..." class="form-control" name="redirectLink" required>
-                        </div>
-                        <div class="text-right">
-                            <input type="submit" class="btn btn-primary" value="Create Shortlink">
-                        </div>
-                    </form>
+                <div class="col-md-6">
+                    <table id="shortlink-stats-by-clicks-table" class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Shortlink</th>
+                                <th scope="col">Clicks</th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
+                <div class="col-md-12 mt-sm">
+                    <h3>Distribution by Referrals</h3>
+                </div>
+                <div class="col-md-6">
+                    <canvas id="shortlink-stats-by-referer-chart"></canvas>
+                </div>
+                <div class="col-md-6">
+                    <table id="shortlink-stats-by-referer-table" class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Referers</th>
+                                <th scope="col">Clicks</th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
+                <div class="col-md-12 mt-sm">
+                    <h3>Distribution by Country</h3>
+                </div>
+                <div class="col-md-6">
+                    <canvas id="shortlink-stats-by-country-chart"></canvas>
+                </div>
+                <div class="col-md-6">
+                    <table id="shortlink-stats-by-country-table" class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Referers</th>
+                                <th scope="col">Clicks</th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
                 </div>
             </div>
+        </div>     
+        <div class="tab-pane fade" id="shortlink-list-container" role="tabpanel" aria-labelledby="shortlink-list">
+            <h5>Your Shortlinks</h5>
+            <div id="shortlink-list-view">
+            </div>
+        </div>
+        <div class="tab-pane fade" id="shortlink-new-container" role="tabpanel" aria-labelledby="shortlink-new">
+            <h5>New Shortlink</h5>
+            <form id="add-shortlink-form">
+                <div class="form-group">
+                    <input type="url" placeholder="Paste your link here..." class="form-control" name="redirectLink" required>
+                </div>
+                <div class="text-right">
+                    <input type="submit" class="btn btn-primary" value="Create Shortlink">
+                </div>
+            </form>
         </div>
     </div>
+
+
+    
 </div>
+<!-- <div class="col-md-6">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
+</div> -->
