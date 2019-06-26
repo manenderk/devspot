99b454d8bb8f9d0af3a4aa69c4d3c4edeb2d1bd6
Manender Kumar <manender.kumar@mamsys.com>
2019-06-26 15:05:11 +0530

    added instructions to format code

--- inc/page-partials/code-formatter-page.php
+++ inc/page-partials/code-formatter-page.php
@@ -1,7 +1,9 @@
 <div class="col-sm-12">
 	<div class="row">
 		<div class="col-sm-12">
-			<h3>Format your code</h3>
+            <p>Code formatter can format your HTML, CSS, JavaScript or JSON string.<br>
+            You just need to paste your code in the given box, select your desired configuration and click on format.<br>
+            In the next moment you'll get your code formatted.</p>
 		</div>
     </div>
     <div class="row code-format-inputs">
@@ -80,6 +82,8 @@
         <div class="col-sm-12 text-center">
             <button id="format-code" class="btn btn-primary inline-element" type="button">Format</button>
         </div>
+    </div>
+    <div class="row">
         <div class="col-sm-12">
             <div id="formatted-code-box" class="form-group">
                 <label for="formatted-code">Your formatted code...</label>
@@ -93,6 +97,20 @@
             <button id="copy-formatted-code" class="btn btn-primary" type="button">Copy</button>
         </div>
 	</div>
+    <div class="row mt-5">
+        <div class="col-sm-12">
+            <h4>Need Help?</h4>
+            <p>Just follow the instructions below</p>
+            <ol>
+                <li>Paste your code in the first input box which says <b>Paste your code here... </b>.</li>
+                <li>Select your code format. If its HTML than click on <b>HTML</b>, if its Javascript then Click on <b>JavaScript</b> and so on.</li>
+                <li>Select your desired spacing level.</li>
+                <li>If you want tabs instead of space, or not sure which one to choose, just click on <b>Use tabs Instead.</b></li>
+                <li>Now you just have to click on <b>Format</b> button</li>
+            </ol>
+            <p><b>For your privacy, we never store your code. All the work done at your machine only.</b></p>
+        </div>
+    </div>
 </div>
 <script src="<?php echo get_template_directory_uri() . '/build/vendor/prettier/standalone.js' ?>" async></script>
 <script src="<?php echo get_template_directory_uri() . '/build/vendor/prettier/parser-html.js' ?>" async></script>
