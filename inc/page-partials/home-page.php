909f706fcabf68aae9feea7f1811b1d7dc62439d
Manender Kumar <manender.kumar@mamsys.com>
2019-06-28 15:13:28 +0530

    added home page content

--- inc/page-partials/home-page.php
+++ inc/page-partials/home-page.php
@@ -1,107 +1,249 @@
-<section class="section section-lg pt-lg-0 mt--200  ">
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-lg-12">
-        <div class="row row-grid">
-          <div class="col-md-4">
-            <div class="card card-lift--hover shadow border-0">
-              <div class="card-body">
-                <img class="img-fluid mb-4"
-                  src="<?php echo get_site_url() ?>/wp-content/themes/devspot/build/images/devspot-tools.jpg">
-                <h6 class="text-primary text-uppercase">SOME TOOLS</h6>
-                <p class="description mt-3">Here we have some tools for you to help you in your web projects. You don't
-                  need to go anywhere else if you need some minifier, a color guide or any other thing that you need to
-                  develop your project.</p>
-                <a href="<?php echo get_site_url()."/tools/" ?>" class="btn btn-primary mt-4">Learn more</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="card card-lift--hover shadow border-0">
-              <div class="card-body">
-                <img class="img-fluid mb-4"
-                  src="<?php echo get_site_url() ?>/wp-content/themes/devspot/build/images/devspot-blog.jpg">
-                <h6 class="text-success text-uppercase">A BLOG</h6>
-                <p class="description mt-3">A blog is here, where you can read a develop experience or maybe you can
-                  catch a new technology that can make development of your next project more efficient with full of fun.
-                </p>
-                <a href="<?php echo get_site_url()."/blog/" ?>" class="btn btn-success mt-4">Learn more</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="card card-lift--hover shadow border-0">
-              <div class="card-body">
-                <img class="img-fluid mb-4"
-                  src="<?php echo get_site_url() ?>/wp-content/themes/devspot/build/images/devspot-contact.jpg">
-                <h6 class="text-warning text-uppercase">SOMETHING ELSE</h6>
-                <p class="description mt-3">If you are looking for something else, then we are open to suggestions. Want
-                  a new tutorial or content, whatever it is. You can always ask us and we will try to put it in front of
-                  you.</p>
-                <a href="<?php echo get_site_url()."/feedback/" ?>" class="btn btn-warning mt-4">Learn more</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+<section class="section pt-lg-0 mt--200  ">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-12">
+				<div class="row row-grid">
+					<div class="col-md-4">
+						<div class="card card-lift--hover shadow border-0">
+							<div class="card-body">
+								<img class="img-fluid mb-4"
+									src="<?php echo get_site_url() ?>/wp-content/themes/devspot/build/images/devspot-tools.jpg">
+								<h6 class="text-primary text-uppercase">SOME TOOLS</h6>
+								<p class="description mt-3">Here we have some tools for you to help you in your web
+									projects. You don't need to go anywhere else if you need some minifier, a color
+									guide or any other thing
+									that you need to develop your project.</p>
+								<a href="<?php echo get_site_url()."/tools/" ?>"
+									class="btn btn-primary mt-4">Learn more</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="card card-lift--hover shadow border-0">
+							<div class="card-body">
+								<img class="img-fluid mb-4"
+									src="<?php echo get_site_url() ?>/wp-content/themes/devspot/build/images/devspot-blog.jpg">
+								<h6 class="text-success text-uppercase">A BLOG</h6>
+								<p class="description mt-3">A blog is here, where you can read a develop experience or
+									maybe you can catch a new technology that can make development of your next project
+									more efficient with full of fun.
+								</p>
+								<a href="<?php echo get_site_url()."/blog/" ?>"
+									class="btn btn-success mt-4">Learn more</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="card card-lift--hover shadow border-0">
+							<div class="card-body">
+								<img class="img-fluid mb-4"
+									src="<?php echo get_site_url() ?>/wp-content/themes/devspot/build/images/devspot-contact.jpg">
+								<h6 class="text-warning text-uppercase">SOMETHING ELSE</h6>
+								<p class="description mt-3">If you are looking for something else, then we are open to
+									suggestions. Want
+									a new tutorial or content, whatever it is. You can always ask us and we will try to
+									put it in front of you.</p>
+								<a href="<?php echo get_site_url()."/feedback/" ?>"
+									class="btn btn-warning mt-4">Learn more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
+
 <section class="section section-lg bg-secondary">
-  <div class="container">
-    <div class="row row-grid align-items-center">
-      <div class="col-md-6">
-        <div class="pr-md-5">
-          <div class="icon icon-lg icon-shape icon-shape-success shadow rounded-circle mb-5">
-            <i class="fa fa-cogs"></i>
-          </div>
-          <h3>Upcoming feature - Url Shortner</h3>
-          <p>No more using very long urls. Here we got you covered by providing our own URL shortner. Want anaytics as
-            well? Don't worry we got it also.</p>
-          <ul class="list-unstyled mt-5">
-            <li class="py-2">
-              <div class="d-flex align-items-center">
-                <div>
-                  <div class="badge badge-circle badge-success mr-3">
-                    <i class="fa fa-cog"></i>
-                  </div>
-                </div>
-                <div>
-                  <h6 class="mb-0">No Limits</h6>
-                </div>
-              </div>
-            </li>
-            <li class="py-2">
-              <div class="d-flex align-items-center">
-                <div>
-                  <div class="badge badge-circle badge-success mr-3">
-                    <i class="fa fa-cog"></i>
-                  </div>
-                </div>
-                <div>
-                  <h6 class="mb-0">Track your Traffic</h6>
-                </div>
-              </div>
-            </li>
-            <li class="py-2">
-              <div class="d-flex align-items-center">
-                <div>
-                  <div class="badge badge-circle badge-success mr-3">
-                    <i class="fa fa-cog"></i>
-                  </div>
-                </div>
-                <div>
-                  <h6 class="mb-0">Get Shortning in minutes</h6>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <img src="<?php echo get_site_url() ?>/wp-content/themes/devspot/build/images/devspot-url-shortner.png"
-          class="img-fluid floating">
-      </div>
-    </div>
-  </div>
+	<div class="container">
+		<div class="row row-grid align-items-center">
+			<div class="col-md-6">
+				<div class="card bg-primary shadow border-0">
+					<img src="<?php echo get_site_url() ?>/wp-content/uploads/2019/06/deevsi-laptop-card.jpg"
+						class="card-img-top" />
+					<blockquote class="card-blockquote">
+						<svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95"
+							class="svg-bg">
+							<polygon points="0,52 583,95 0,95" class="fill-primary"></polygon>
+							<polygon points="0,42 583,95 683,0 0,95" opacity=".2" class="fill-info"></polygon>
+						</svg>
+						<h4 class="display-3 font-weight-bold text-white">The Idea</h4>
+						<p class="lead text-italic text-white">Deevsi is all about transforming ideas into reality. We
+							all developers are full of ideas, but have you ever tried to make somthing from them?</p>
+					</blockquote>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="pl-md-5">
+					<div class="icon icon-lg icon-shape icon-shape-primary shadow rounded-circle mb-5">
+						<i class="fa fa-check text-white"></i>
+					</div>
+					<h3>The Ups and Downs</h3>
+					<p class="lead">So here's the thought. We always have something in our mind, that we desire to
+						become reality. But somehow we got stopped.</p>
+					<p>One of the main thing is that we don't know everything. Many says that they don't know at all or
+						enough so that they can make their idea working.</p>
+					<p>Here comes Deevsi. Where we all can learn together. Share an Idea or discuss things. A place to
+						learn, grow and proove together.</p>
+					<a href="<?php echo get_site_url(); ?>/blog/"
+						class="font-weight-bold text-primary mt-5">Here's a Blog to keep us running.</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="section pb-0 bg-gradient-warning">
+	<div class="container">
+		<div class="row row-grid align-items-center">
+			<div class="col-md-6 order-lg-2 ml-lg-auto">
+				<div class="position-relative pl-md-5">
+					<img src="<?php echo get_site_url() ?>/wp-content/uploads/2019/06/deevsi-banner-vector.png" class="img-center img-fluid">
+				</div>
+			</div>
+			<div class="col-lg-6 order-lg-1">
+				<div class="d-flex px-3">
+					<div>
+						<div class="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-warning">
+							<i class="fa fa-rss"></i>
+						</div>
+					</div>
+					<div class="pl-4">
+						<h4 class="display-3 text-white"><a href="<?php echo get_site_url().'/blog/'?>" class="text-white">Recent from Blog</a></h4>
+						<p class="text-white">What we have till now...</p>
+					</div>
+				</div>
+
+				<?php 
+				wp_reset_query();
+				$recent_posts = wp_get_recent_posts(array(
+					'numberposts' => 2, // Number of recent posts thumbnails to display
+					'post_status' => 'publish' // Show only the published posts
+				));
+				wp_reset_query();
+				?>
+				<?php if (!empty($recent_posts)) : ?>
+					<?php foreach ($recent_posts as $post) : ?>
+						<div class="card shadow shadow-lg--hover mt-5">
+							<div class="card-body">
+								<div class="d-flex px-3">
+									<div>
+										<div class="icon icon-shape bg-gradient-warning rounded-circle text-white">
+											<i class="fa fa-angle-right"></i>
+										</div>
+									</div>
+									<div class="pl-4">
+										<h5 class="title text-warning"><?php echo $post['post_title'] ?></h5>
+										<p><?php echo substr(strip_tags($post['post_content']),0,100) ?>...</p>
+										<a href="<?php echo get_permalink($post['ID']) ?>" class="text-warning">Learn more</a>												
+									</div>
+								</div>
+							</div>
+						</div>
+					<?php endforeach; ?>
+				<?php else: ?>
+					<div class="card shadow shadow-lg--hover mt-5">
+						<div class="card-body">
+							<div class="d-flex px-3">
+								<div>
+									<div class="icon icon-shape bg-gradient-warning rounded-circle text-white">
+										<i class="fa fa-angle-right"></i>
+									</div>
+								</div>
+								<div class="pl-4">
+									<h5 class="title text-warning">No posts yet...</h5>
+									<p>We will publish something new soon...</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				<?php endif; ?>				
+			</div>
+		</div>
+	</div>
+	<!-- SVG separator -->
+	<div class="separator separator-bottom separator-skew zindex-100">
+		<svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1"
+			xmlns="http://www.w3.org/2000/svg">
+			<polygon class="fill-white" points="2560 0 2560 100 0 100"></polygon>
+		</svg>
+	</div>
+</section>
+
+<section class="section section-lg">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<h3 class="display-2">We published some <a href="<?php echo get_site_url() ?>/tools/" class="text-primary">Tools</a> to make development easier for you...</h3>
+			</div>
+			<div class="col-sm-12 justify-content-center">
+				<a class="btn btn-1 btn-outline-primary tool-btn" href="<?php echo get_site_url() ?>/tools/code-formatter/">Code Formatter</a>
+				<a class="btn btn-1 btn-outline-primary tool-btn" href="<?php echo get_site_url() ?>/tools/minify-css-javascript/"> Minify CSS & JavaScript </a>
+				<a class="btn btn-1 btn-outline-primary tool-btn" href="<?php echo get_site_url() ?>/tools/aspect-ratio-calculator/">Aspect Ratio Calculator</a>
+				<a class="btn btn-1 btn-outline-primary tool-btn" href="<?php echo get_site_url() ?>/tools/color-converter/">Color Code Converter</a>
+			</div>
+			<div class="col-sm-12 text-right">
+				<h3 class="display-4 text-right">And many more coming...</h3>
+			</div>
+		</div>
+	</div>
+</section>	
+
+<section class="section section-lg">
+	<div class="container">
+		<div class="row row-grid align-items-center">
+			<div class="col-md-6">
+				<div class="pr-md-5">
+					<div class="icon icon-lg icon-shape icon-shape-success shadow rounded-circle mb-5">
+						<i class="fa fa-cogs"></i>
+					</div>
+					<h3>Upcoming feature - Url Shortner</h3>
+					<p>No more using very long urls. Here we got you covered by providing our own URL shortner. Want
+						anaytics as well? Don't worry we got it also.</p>
+					<ul class="list-unstyled mt-5">
+						<li class="py-2">
+							<div class="d-flex align-items-center">
+								<div>
+									<div class="badge badge-circle badge-success mr-3">
+										<i class="fa fa-cog"></i>
+									</div>
+								</div>
+								<div>
+									<h6 class="mb-0">No Limits</h6>
+								</div>
+							</div>
+						</li>
+						<li class="py-2">
+							<div class="d-flex align-items-center">
+								<div>
+									<div class="badge badge-circle badge-success mr-3">
+										<i class="fa fa-cog"></i>
+									</div>
+								</div>
+								<div>
+									<h6 class="mb-0">Track your Traffic</h6>
+								</div>
+							</div>
+						</li>
+						<li class="py-2">
+							<div class="d-flex align-items-center">
+								<div>
+									<div class="badge badge-circle badge-success mr-3">
+										<i class="fa fa-cog"></i>
+									</div>
+								</div>
+								<div>
+									<h6 class="mb-0">Get Shortning in minutes</h6>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<img src="<?php echo get_site_url() ?>/wp-content/themes/devspot/build/images/devspot-url-shortner.png"
+					class="img-fluid floating">
+			</div>
+		</div>
+	</div>
 </section>
