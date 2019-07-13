d0e54da0b71f81f1a56ab8fd11f38d7872a1285e
manender <manender369@gmail.com>
2019-07-13 10:06:27 -0400

    production migration to dev

---
+++ inc/page-partials/shortlink.php
@@ -0,0 +1,62 @@
+<div class="col-sm-12 text-center">
+    <h2 class="display-1 text-center">Shortlinks are Live Now</h2>
+</div>
+<div class="col-sm-12 mt-5">
+    <div class="row row-grid align-items-center bg-gradient-primary">
+        <div class="col-md-6 order-lg-2 ml-lg-auto">
+            <div class="position-relative pl-md-5">
+                <img src="https://www.deevsi.com/wp-content/uploads/2019/06/deevsi-banner-vector.png" class="img-center img-fluid lazyloaded" alt="deevsi blog vector">
+            </div>
+        </div>
+        <div class="col-lg-6 order-lg-1">
+            <div class="d-flex px-3">
+                <div>
+                    <div class="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-warning">
+                        <i class="fa fa-rss"></i>
+                    </div>
+                </div>
+                <div class="pl-4">
+                    <h4 class="display-3 text-white"><a href="https://www.deevsi.com/blog/" class="text-white">Recent from Blog</a></h4>
+                    <p class="text-white">What we have till now...</p>
+                </div>
+            </div>
+            <div class="card shadow shadow-lg--hover mt-5">
+                <div class="card-body">
+                    <div class="d-flex px-3">
+                        <div>
+                            <div class="icon icon-shape bg-gradient-warning rounded-circle text-white">
+                                <i class="fa fa-angle-right"></i>
+                            </div>
+                        </div>
+                        <div class="pl-4">
+                            <h5 class="title text-warning">Animate Text using CSS</h5>
+                            <p>It is so easy to animate text using css. Generally, I prefer two options to animate text. Either us...</p>
+                            <a href="https://www.deevsi.com/animate-text-using-css/" class="text-warning">Learn more</a>												
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card shadow shadow-lg--hover mt-5">
+                <div class="card-body">
+                    <div class="d-flex px-3">
+                        <div>
+                            <div class="icon icon-shape bg-gradient-warning rounded-circle text-white">
+                                <i class="fa fa-angle-right"></i>
+                            </div>
+                        </div>
+                        <div class="pl-4">
+                            <h5 class="title text-warning">Why I Prefer CakePHP for building a CMS</h5>
+                            <p>One of my favorite PHP based framework is CakePHP. Whenever there is a need for building a CMS, Cak...</p>
+                            <a href="https://www.deevsi.com/why-i-prefer-cakephp-for-building-a-cms/" class="text-warning">Learn more</a>												
+                        </div>
+                    </div>
+                </div>
+            </div>												
+        </div>
+    </div>
+    <div class="separator separator-bottom separator-skew zindex-100">
+		<svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+			<polygon class="fill-white" points="2560 0 2560 100 0 100"></polygon>
+		</svg>
+	</div>
+</div>
