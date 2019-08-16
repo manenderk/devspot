a6d831f6db6c732e62a792886df610258c3b7f69
manender <manender369@gmail.com>
2019-08-16 03:45:53 -0400

    corrected portfolio image links

--- inc/page-partials/my-portfolio.php
+++ inc/page-partials/my-portfolio.php
@@ -27,49 +27,49 @@
             <div class="grid3d vertical" id="grid3d">
 				<div class="grid-wrap">
 					<div class="grid row">
-						<figure class="col-md-6"><img class="img img-fluid shadow" src="wp-content/uploads/2019/08/mmr-main.png" alt="deevsi manender make my resume"/></figure>
-						<figure class="col-md-6"><img class="img img-fluid shadow" src="wp-content/uploads/2019/08/buildjoy-main.png" alt="deevsi manender buildjoy"/></figure>
-						<figure class="col-md-6"><img class="img img-fluid shadow" src="wp-content/uploads/2019/08/dy-main.png" alt="deevsi manender digital youth"/></figure>
-						<figure class="col-md-6"><img class="img img-fluid shadow" src="wp-content/uploads/2019/08/itcost-main.png" alt="deevsi manender itcost"/></figure>
-						<figure class="col-md-6"><img class="img img-fluid shadow" src="wp-content/uploads/2019/08/ldc-main.png" alt="deevsi manender london doctors clinic"/></figure>
+						<figure class="col-md-6"><img class="img img-fluid shadow" src="/wp-content/uploads/2019/08/mmr-main.png" alt="deevsi manender make my resume"/></figure>
+						<figure class="col-md-6"><img class="img img-fluid shadow" src="/wp-content/uploads/2019/08/buildjoy-main.png" alt="deevsi manender buildjoy"/></figure>
+						<figure class="col-md-6"><img class="img img-fluid shadow" src="/wp-content/uploads/2019/08/dy-main.png" alt="deevsi manender digital youth"/></figure>
+						<figure class="col-md-6"><img class="img img-fluid shadow" src="/wp-content/uploads/2019/08/itcost-main.png" alt="deevsi manender itcost"/></figure>
+						<figure class="col-md-6"><img class="img img-fluid shadow" src="/wp-content/uploads/2019/08/ldc-main.png" alt="deevsi manender london doctors clinic"/></figure>
 						
 					</div>
 				</div><!-- /grid-wrap -->
 				<div class="content">
 					<div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/mmr-main.png" class="img img-fluid" alt="deevsi manender make my resume"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/mmr-1.png" class="img img-fluid" alt="deevsi manender make my resume"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/mmr-2.png" class="img img-fluid" alt="deevsi manender make my resume"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/mmr-3.png" class="img img-fluid" alt="deevsi manender make my resume"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/mmr-4.png" class="img img-fluid" alt="deevsi manender make my resume"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/mmr-main.png" class="img img-fluid" alt="deevsi manender make my resume"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/mmr-1.png" class="img img-fluid" alt="deevsi manender make my resume"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/mmr-2.png" class="img img-fluid" alt="deevsi manender make my resume"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/mmr-3.png" class="img img-fluid" alt="deevsi manender make my resume"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/mmr-4.png" class="img img-fluid" alt="deevsi manender make my resume"/></div>
 						<!-- <p class="grid-item-content-text">The only people for me are the mad ones, the ones who are mad to live, mad to talk, mad to be saved, desirous of everything at the same time, the ones who never yawn or say a commonplace thing, but burn, burn, burn like fabulous yellow roman candles exploding like spiders across the stars.</p> -->
 					</div>
 					<div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/buildjoy-main.png" class="img img-fluid" alt="deevsi manender buidjoy"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/buildjoy-1.png" class="img img-fluid" alt="deevsi manender buidjoy"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/buildjoy-2.png" class="img img-fluid" alt="deevsi manender buidjoy"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/buildjoy-3.png" class="img img-fluid" alt="deevsi manender buidjoy"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/buildjoy-4.png" class="img img-fluid" alt="deevsi manender buidjoy"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/buildjoy-5.png" class="img img-fluid" alt="deevsi manender buidjoy"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/buildjoy-main.png" class="img img-fluid" alt="deevsi manender buidjoy"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/buildjoy-1.png" class="img img-fluid" alt="deevsi manender buidjoy"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/buildjoy-2.png" class="img img-fluid" alt="deevsi manender buidjoy"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/buildjoy-3.png" class="img img-fluid" alt="deevsi manender buidjoy"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/buildjoy-4.png" class="img img-fluid" alt="deevsi manender buidjoy"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/buildjoy-5.png" class="img img-fluid" alt="deevsi manender buidjoy"/></div>
 					</div>
 					<div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/dy-main.png" class="img img-fluid" alt="deevsi manender digital youth"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/dy-1.png" class="img img-fluid" alt="deevsi manender digital youth"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/dy-2.png" class="img img-fluid" alt="deevsi manender digital youth"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/dy-3.png" class="img img-fluid" alt="deevsi manender digital youth"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/dy-main.png" class="img img-fluid" alt="deevsi manender digital youth"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/dy-1.png" class="img img-fluid" alt="deevsi manender digital youth"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/dy-2.png" class="img img-fluid" alt="deevsi manender digital youth"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/dy-3.png" class="img img-fluid" alt="deevsi manender digital youth"/></div>
 					</div>
 					<div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/itcost-main.png" class="img img-fluid" alt="deevsi manender itcost"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/itcost-1.png" class="img img-fluid" alt="deevsi manender itcost"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/itcost-2.png" class="img img-fluid" alt="deevsi manender itcost"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/itcost-3.png" class="img img-fluid" alt="deevsi manender itcost"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/itcost-main.png" class="img img-fluid" alt="deevsi manender itcost"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/itcost-1.png" class="img img-fluid" alt="deevsi manender itcost"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/itcost-2.png" class="img img-fluid" alt="deevsi manender itcost"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/itcost-3.png" class="img img-fluid" alt="deevsi manender itcost"/></div>
 					</div>
 					<div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/ldc-main.png" class="img img-fluid" alt="deevsi manender london doctors clinic"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/ldc-1.png" class="img img-fluid" alt="deevsi manender london doctors clinic"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/ldc-2.png" class="img img-fluid" alt="deevsi manender london doctors clinic"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/ldc-3.png" class="img img-fluid" alt="deevsi manender london doctors clinic"/></div>
-						<div class="grid-item-content-image"><img src="wp-content/uploads/2019/08/ldc-4.png" class="img img-fluid" alt="deevsi manender london doctors clinic"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/ldc-main.png" class="img img-fluid" alt="deevsi manender london doctors clinic"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/ldc-1.png" class="img img-fluid" alt="deevsi manender london doctors clinic"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/ldc-2.png" class="img img-fluid" alt="deevsi manender london doctors clinic"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/ldc-3.png" class="img img-fluid" alt="deevsi manender london doctors clinic"/></div>
+						<div class="grid-item-content-image"><img src="/wp-content/uploads/2019/08/ldc-4.png" class="img img-fluid" alt="deevsi manender london doctors clinic"/></div>
 					</div>
 					<span class="loading"></span>
 					<span class="icon close-content"><i class="fa fa-times"></i></span>
